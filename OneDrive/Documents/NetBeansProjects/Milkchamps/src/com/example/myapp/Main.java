7b68c2809e7c9defbb4917ae9be18c4d00127ee9
alyarubyjune <rubysclev@gmail.com>
2024-06-28 21:27:03 +0700

    first commit

---
+++ OneDrive/Documents/NetBeansProjects/Milkchamps/src/com/example/myapp/Main.java
@@ -0,0 +1,10 @@
+package com.example.myapp;
+
+//import com.example.myapp.Homepage;
+
+public class Main {
+    public static void main(String[] args) {
+        Homepage frame = new Homepage();
+        frame.setVisible(true);
+    }
+}
